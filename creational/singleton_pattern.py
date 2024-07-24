0ceb33ed6327c8f9d7e08fc7e128f87c4661d2f5
alimhtsai <48788292+alimhtsai@users.noreply.github.com>
2024-07-24 10:26:41 -0700

    add thread-safe metaclass implementation

--- creational/singleton_pattern.py
+++ creational/singleton_pattern.py
@@ -1,4 +1,5 @@
 import threading
+from datetime import datetime
 from typing import Any
 
 
@@ -23,7 +24,7 @@ class ClassicSingleton:
 
 
 '''simple python way'''
-class Singleton:
+class SimpleSingleton:
     # class-level variable to store single class instance
     _instance = None
 
@@ -76,6 +77,7 @@ class MetaSingletonEager(type):
         # eager loading
         super().__init__(name, bases, dct)
         cls._instances[cls] = super().__call__()
+        print('initializing <super>...')
 
     # return the singleton instance
     def __call__(cls: Any, *args: Any, **kwargs: Any) -> Any:
@@ -85,6 +87,7 @@ class MetaSingletonEager(type):
 '''the implementation of MetaSingletonEager'''
 class ConcreteMetaSingletonEager(metaclass=MetaSingletonEager):
     def __init__(self) -> None:
+        print('initializing <child>...')
         pass
 
 
@@ -106,17 +109,71 @@ class ThreadSafeSingleton:
         return cls._instance
 
 
+'''thread-safe metaclass implementation'''
+class MetaThreadSafeSingleton(type):
+    _instances: dict = {}
+    _lock = threading.Lock()
+
+    def __call__(cls, *args, **kwargs):
+
+        # acquire the lock to ensure thread safety
+        with cls._lock:
+            if cls not in cls._instances:
+                cls._instances[cls] = super().__call__(*args, **kwargs)
+            return cls._instances[cls]
+
+
+'''the implementation of MetaThreadSafeSingleton'''
+class ConcreteMetaThreadSafeSingleton(metaclass=MetaThreadSafeSingleton):
+    def time(self):
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+
+
+############################################################################
+# Usage
+############################################################################
+
+
 # classic GoF implementation
-s1 = ClassicSingleton.get_instance()
+s1_1 = ClassicSingleton.get_instance()
+s1_2 = ClassicSingleton.get_instance()
+print(s1_1 is s1_2) # expected True
 
 # simple python way
-s2 = Singleton()
+s2_1 = SimpleSingleton()
+s2_2 = SimpleSingleton()
+print(s2_1 is s2_2) # expected True
 
 # best singleton implementation with lazy initialization
-s3 = ConcreteMetaSingletonLazy()
+s3_1 = ConcreteMetaSingletonLazy()
+s3_2 = ConcreteMetaSingletonLazy()
+print(s3_1 is s3_2) # expected True
 
 # best singleton implementation with eager loading
-s4 = ConcreteMetaSingletonEager()
+s4_1 = ConcreteMetaSingletonEager()
+s4_2 = ConcreteMetaSingletonEager()
+print(s4_1 is s4_2) # expected True
 
 # thread-safe implementation
 s5 = ThreadSafeSingleton()
+
+# thread-safe metaclass implementation
+def get_singleton_instance():
+    s = ConcreteMetaThreadSafeSingleton()
+    print(s.time())
+
+# create a list to store threads
+threads = []
+
+# create 10 thread objects, appending each to the threads list
+for i in range(10):
+    t = threading.Thread(target=get_singleton_instance)
+    threads.append(t)
+
+# start each thread in the threads list
+for t in threads:
+    t.start()
+
+# wait for each thread to finish
+for t in threads:
+    t.join()
